858cf2d56c9a8f76eccc064937b23f225b279c4e
Galih Anggi Satriawan (galihsatriawan) <satriawan.galih99@gmail.com>
2017-09-08 16:20:51 +0700

    Create If Then Else.cpp

---
+++ IA TOKI/Pemrograman Dasar/Percabangan/If Then Else.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	long a;
+	cin>>a;
+	if(a>0) cout<<"positif"<<endl;
+	else if(a<0) cout<<"negatif"<<endl;
+	else cout<<"nol"<<endl;
+	return 0;
+}
