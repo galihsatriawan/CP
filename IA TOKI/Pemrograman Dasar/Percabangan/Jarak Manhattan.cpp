73a2e9b73aae6cbae0f4296866791e6dcf468643
Galih Anggi Satriawan (galihsatriawan) <satriawan.galih99@gmail.com>
2017-09-08 16:22:09 +0700

    Create Jarak Manhattan.cpp

---
+++ IA TOKI/Pemrograman Dasar/Percabangan/Jarak Manhattan.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+	long x1,x2,y1,y2;
+	cin>>x1>>y1>>x2>>y2;
+	long hasil;
+	hasil= abs(x1-x2)+abs(y1-y2);
+	cout<<hasil<<endl;
+	return 0;
+}
