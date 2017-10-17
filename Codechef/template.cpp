00bba05fe564243a5500f387f740affb5c7ab370
Galih Anggi Satriawan (galihsatriawan) <satriawan.galih99@gmail.com>
2017-10-17 13:07:22 +0700

    Add files via upload

---
+++ Codechef/template.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <stdio.h>
+#include <vector>
+#include <cmath>
+#define FORa(i,a,ak) for(i=a;i<=ak;i++)
+#define FOR(i,n) for(i=1;i<=n;i++)
+#define pf printf
+#define sf scanf
+using namespace std;
+
+
+int main(){
+
+
+
+
+
+	return 0;
+}
