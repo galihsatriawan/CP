8763e516fd7ed2467975613ad0e491f9f38d0daf
Galih Anggi Satriawan (galihsatriawan) <satriawan.galih99@gmail.com>
2017-10-17 07:52:32 +0700

    Create Add Two Numbers.cpp

---
+++ Codechef/Add Two Numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+#include <stdio.h>
+#include <vector>
+#include <cmath>
+#define FORa(i,a,ak) for(i=a;i<=ak;i++)
+#define FOR(i,n) for(i=1;i<=n;i++)
+#define pf printf
+#define sf scanf
+using namespace std;
+
+
+int main(){
+	int i,j,a,b,t;
+	cin>>t;
+	FOR(i,t){
+		cin>>a>>b;
+		cout<<(a+b)<<endl;
+	}
+	return 0;
+}
