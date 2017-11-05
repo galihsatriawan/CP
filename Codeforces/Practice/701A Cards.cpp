f99c210381506bf3a105ccad8992ff20164a2d1e
Galih Anggi Satriawan (galihsatriawan) <satriawan.galih99@gmail.com>
2017-11-05 10:14:10 +0700

    Add with friend's little notebook

---
+++ Codeforces/Practice/701A Cards.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N=1e3+5;
+
+#define sf scanf
+#define pf printf
+#define FOR(i,n) for(i=1;i<=n;i++)
+#define FORa(i,ai,aj) for(i=ai;i<=aj;i++)
+
+int main(){
+    pair<int,int> hasil[N];
+    int n,i,j;
+    cin>>n;
+    bool hasCh[n+1];
+    memset(hasCh,false,sizeof hasCh);
+    int a[n+1];
+    int jumlah=0;
+    FOR(i,n){
+        cin>>a[i];
+        jumlah+=a[i];
+    }
+    int index=0;
+    int rata=jumlah/(n/2);
+    FOR(i,n-1){
+        if(!hasCh[i]){
+            hasCh[i]=true;
+            FORa(j,i+1,n){
+                if((!hasCh[j])&&(a[i]+a[j]==rata)){
+                    hasil[++index]=make_pair(i,j);
+                    hasCh[j]=true;
+                    break;
+                }
+            }
+        }
+
+    }
+    FOR(i,index){
+        cout<<hasil[i].first<<" "<<hasil[i].second<<endl;
+    }
+	return 0;
+}
---
+++ Codeforces/Practice/721A One-dimensional Japanese Crossword.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N=1e3+5;
+
+#define sf scanf
+#define pf printf
+#define FOR(i,n) for(i=1;i<=n;i++)
+#define FORa(i,ai,aj) for(i=ai;i<=aj;i++)
+
+int main(){
+    int n,i,j,n_b[N];
+    string japane;
+    sf("%d",&n);
+    cin>>japane;
+    int panjang=japane.size()-1;
+    int count_o=0;
+    while(japane.find("B")<=panjang){
+        int posisi=japane.find("B");
+        panjang=japane.size()-1;
+        count_o++;
+        n_b[count_o]=1;
+        while(japane[posisi]==japane[posisi+1]) {
+                posisi++;
+                n_b[count_o]++;
+        }
+        japane=japane.substr(posisi+1,panjang-posisi+1);
+    }
+    cout<<count_o<<endl;
+    FOR(i,count_o){
+        if(i==count_o) cout<<n_b[i]<<endl;
+        else cout<<n_b[i]<<" ";
+    }
+	return 0;
+}
