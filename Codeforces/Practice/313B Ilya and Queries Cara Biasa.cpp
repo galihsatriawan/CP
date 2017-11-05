509da77d986781db27c3076ada2707c6f61ee16f
Galih Anggi Satriawan (galihsatriawan) <satriawan.galih99@gmail.com>
2017-11-05 14:14:31 +0700

    Try with pure DP Memoize but the result is TLE

---
+++ Codeforces/Practice/313B Ilya and Queries Cara Biasa.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N=1e5+5;
+
+#define sf scanf
+#define pf printf
+#define FOR(i,n) for(i=1;i<=n;i++)
+#define FORa(i,ai,aj) for(i=ai;i<=aj;i++)
+
+int main(){
+    int q,j,i,b_titik[N],l,r;
+    string kode;
+    cin>>kode;
+    b_titik[0]=0;
+    int panjang=kode.size();
+    FORa(i,1,panjang-1){
+        b_titik[i]=b_titik[i-1]+(kode[i]==kode[i-1]);
+    }
+    sf("%d",&q);
+    while(q--){
+        sf("%d %d",&l,&r);
+        cout<<b_titik[r-1]-b_titik[l-1]<<endl;
+    }
+	return 0;
+}
---
+++ Codeforces/Practice/313B Ilya and Queries Cara DP (tapi TLE).cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N=1e5+5;
+
+#define sf scanf
+#define pf printf
+#define FOR(i,n) for(i=1;i<=n;i++)
+#define FORa(i,ai,aj) for(i=ai;i<=aj;i++)
+map< pair<int,int>,int> hasil;
+set< pair<int,int> > exist;
+string kode;
+//DP biasa
+/*int dp(int i,int j){
+    int sama=0;
+    if(j<=i) return 0;
+    else {
+            if (kode[j-1]==kode[j]) sama=1;
+            return (dp(i,j-1)+sama);
+    }
+}*/
+//DP Memoize
+int dp(int i,int j){
+    int sama=0;
+
+    //jika belum ada
+    if(j<=i) return 0;
+    else{
+        if(kode[j-1]==kode[j]) sama = 1;
+        //ketika belum ada
+        if(exist.count({i,j})==0){
+                //tandai sudah ada
+            exist.insert({i,j});
+            hasil.insert({{i,j},dp(i,j-1)+sama});
+            return hasil.at({i,j});
+        }else { //jika sudah ada
+            return hasil.at({i,j});
+        }
+    }
+
+}
+int main(){
+
+    //hasil.insert({{2,3},3});
+    //cout<<exist.count({2,3});
+    //cout<<;
+
+    int q,i,l,r;
+    int h_q[N];
+    cin>>kode;
+    sf("%d",&q);
+    FOR(i,q){
+        sf("%d %d",&l,&r);
+        h_q[i]=dp(l-1,r-1);
+    }
+    FOR(i,q){
+    cout<<h_q[i]<<endl;
+    }
+	return 0;
+}
